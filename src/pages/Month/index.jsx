d98b36db069767e755c9f57281850a0eebaa5db5
Robert-J-WANG <robin.nz420@gmail.com>
2023-11-25 20:27:59 +1300

    11-月度账单-切换时间的回显和格式化

--- src/pages/Month/index.jsx
+++ src/pages/Month/index.jsx
@@ -2,14 +2,24 @@ import { NavBar, DatePicker } from "antd-mobile";
 import "./index.scss";
 import classNames from "classnames";
 import { useState } from "react";
+import dayjs from "dayjs";
 
 const Month = () => {
   // 控制时间弹窗的打开和关闭
   const [dateVisible, setDateVisible] = useState(false);
+  // 显示时间的变量
+  const [currentDate, setCurrentDate] = useState(
+    dayjs(new Date()).format("YY-MM") //初始化时间为当前时间,并进行格式化
+  );
   // 点击确认的回调
-  const onComfirm = () => {
+  const onComfirm = (date) => {
     // 关闭时间弹窗
     setDateVisible(false);
+    // 设置所选时间
+    console.log(date);
+    // 格式化时间弹框里的时间，并设置为所选时间
+    const formatDate = dayjs(date).format("YYYY-MM");
+    setCurrentDate(formatDate);
   };
   return (
     <div className="monthlyBill">
@@ -21,7 +31,8 @@ const Month = () => {
           {/* 时间切换区域 */}
           <div className="date" onClick={() => setDateVisible(true)}>
             <span className="text">
-              {2023 | 3}
+              {/* jsx不能渲染object对象，+‘’ 转化为字符串 */}
+              {currentDate + ""}
               月账单
             </span>
             {/* 箭头朝上还是朝下？ */}
