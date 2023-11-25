22e986f1167b54e4a75987178ff8c933d2c2eb4f
Robert-J-WANG <robin.nz420@gmail.com>
2023-11-25 16:26:53 +1300

    07-rkt异步获取账单数据

--- src/pages/Layout/index.jsx
+++ src/pages/Layout/index.jsx
@@ -1,9 +1,15 @@
+import { getBillList } from "@/store/Modules/billStore";
 import { Button } from "antd-mobile";
-import React from "react";
-import { useSelector } from "react-redux";
+import React, { useEffect } from "react";
+import { useDispatch, useSelector } from "react-redux";
 import { Outlet } from "react-router-dom";
 
 export const Layout = () => {
+  // 页面初次渲染时，触发异步函数，更新并获取状态数据
+  const dispatch = useDispatch();
+  useEffect(() => {
+    dispatch(getBillList());
+  }, [dispatch]);
   return (
     <div>
       <Outlet />
--- src/store/Modules/billStore.js
+++ src/store/Modules/billStore.js
@@ -1,10 +1,11 @@
 import { createSlice } from "@reduxjs/toolkit";
+import axios from "axios";
 
 const billStore = createSlice({
   name: "bill",
   // 静态状态数据
   initialState: {
-    billList: [1, 2],
+    billList: [],
   },
   // 同步：更新状态的方法
   reducers: {
@@ -16,6 +17,18 @@ const billStore = createSlice({
 
 // 解构出状态creater对象
 const { setBillList } = billStore.actions;
-const billReducer = billStore.reducer;
 
+// 编写异步方法
+const getBillList = () => {
+  return async (dispatch) => {
+    // 获取异步数据
+    const res = await axios.get("http://localhost:8888/ka");
+    // 触发同步方法更行状态数据
+    dispatch(setBillList(res.data));
+  };
+};
+// 导出异步方法
+export { getBillList };
+
+const billReducer = billStore.reducer;
 export default billReducer;
