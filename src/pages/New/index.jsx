1ddf54b9aee8db9d553f275d3559aed7d9f0a710
Robert-J-WANG <robin.nz420@gmail.com>
2023-11-26 17:14:31 +1300

    23-新增账单-新增表单功能实现

--- src/pages/New/index.jsx
+++ src/pages/New/index.jsx
@@ -6,6 +6,8 @@ import { billListData } from "@/contants";
 import dayjs from "dayjs";
 import { useNavigate } from "react-router-dom";
 import { useState } from "react";
+import { addBillList } from "@/store/Modules/billStore";
+import { useDispatch } from "react-redux";
 
 const New = () => {
   const navigate = useNavigate();
@@ -13,6 +15,31 @@ const New = () => {
   // 9.1 控制收入支出的状态
   // pay-支出，income-收入
   const [billType, setBillType] = useState("pay");
+
+  /* ---------------------- 10.保存账单功能 --------------------- */
+  // 10.1 收集money
+  const [money, setMoney] = useState(0);
+  // 10.2 使用受控组件，收集money数据
+  const changeMoney = (value) => {
+    setMoney(value);
+  };
+  // 10.3  收集账单类型
+  const [useFor, setUseFor] = useState("");
+  // 10.6 使用useDispatch钩子调用异步方法
+  const dispatch = useDispatch();
+
+  // 10.5 收集表单数据
+  const saveBill = () => {
+    const data = {
+      type: billType,
+      money: billType === "pay" ? -money : +money,
+      date: new Date(),
+      useFor: useFor,
+    };
+    console.log(data);
+    // 10.7 提交异步方法，保存账单
+    dispatch(addBillList(data));
+  };
 
   return (
     <div className="keepAccounts">
@@ -59,7 +86,9 @@ const New = () => {
                 className="input"
                 placeholder="0.00"
                 type="number"
-                value={100}
+                // 10.2 使用受控组件，收集money数据
+                value={money}
+                onChange={changeMoney}
               />
               <span className="iconYuan">¥</span>
             </div>
@@ -78,7 +107,12 @@ const New = () => {
                 {item.list.map((item) => {
                   return (
                     // selected
-                    <div className={classNames("item", "")} key={item.type}>
+                    <div
+                      className={classNames("item", "")}
+                      key={item.type}
+                      // 10.4  点击图标，收集账单类型
+                      onClick={() => setUseFor(item.type)}
+                    >
                       <div className="icon">
                         <Icon type={item.type} />
                       </div>
@@ -93,7 +127,9 @@ const New = () => {
       </div>
 
       <div className="btns">
-        <Button className="btn save">保 存</Button>
+        <Button className="btn save" onClick={saveBill}>
+          保 存
+        </Button>
       </div>
     </div>
   );
--- src/store/Modules/billStore.js
+++ src/store/Modules/billStore.js
@@ -12,11 +12,15 @@ const billStore = createSlice({
     setBillList(state, action) {
       state.billList = action.payload;
     },
+
+    addBill(state, action) {
+      state.billList.push(action.payload);
+    },
   },
 });
 
 // 解构出状态creater对象
-const { setBillList } = billStore.actions;
+const { setBillList, addBill } = billStore.actions;
 
 // 编写异步方法
 const getBillList = () => {
@@ -27,8 +31,18 @@ const getBillList = () => {
     dispatch(setBillList(res.data));
   };
 };
+// 编写异步add方法
+const addBillList = (data) => {
+  return async (dispatch) => {
+    // 获取异步数据
+    const res = await axios.post("http://localhost:8888/ka", data);
+    // 触发同步方法更行状态数据
+    dispatch(addBill(res.data));
+  };
+};
+
 // 导出异步方法
-export { getBillList };
+export { getBillList, addBillList };
 
 const billReducer = billStore.reducer;
 export default billReducer;
