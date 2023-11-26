c86249de46aeb0fa1971c453f0be6ce675594c39
Robert-J-WANG <robin.nz420@gmail.com>
2023-11-26 15:30:39 +1300

    19-月度账单-列表区域-点击切换详情列表的显示和隐藏

--- src/pages/Month/components/DayBill/index.jsx
+++ src/pages/Month/components/DayBill/index.jsx
@@ -1,6 +1,6 @@
 import classNames from "classnames";
 import "./index.scss";
-import { useMemo } from "react";
+import { useMemo, useState } from "react";
 import { billTypeToName } from "@/contants";
 // 6.4 接收dailyBill数据并渲染
 const DailyBill = ({ date, dailyBillList }) => {
@@ -23,6 +23,10 @@ const DailyBill = ({ date, dailyBillList }) => {
     };
   }, [dailyBillList]);
 
+  /* ----------------- 7.箭头的翻转，详情列表的显示和隐藏 ----------------- */
+  // 7.1 状态变量控制显隐和箭头翻转
+  const [visible, setVisible] = useState(false);
+
   return (
     <div className={classNames("dailyBill")}>
       {/* 单日列表统计区 */}
@@ -30,7 +34,12 @@ const DailyBill = ({ date, dailyBillList }) => {
         <div className="dateIcon">
           <span className="date">{date}</span>
           {/* expand 有这个类名 展开的箭头朝上的样子 */}
-          <span className={classNames("arrow")}></span>
+          <span
+            // 7.3 visible值控制箭头的翻转
+            className={classNames("arrow", visible && "expand")}
+            // 7.2 点击箭头，visible值取反
+            onClick={() => setVisible(!visible)}
+          ></span>
         </div>
         <div className="oneLineOverview">
           <div className="pay">
@@ -48,23 +57,26 @@ const DailyBill = ({ date, dailyBillList }) => {
         </div>
       </div>
       {/* 单日列表详细显示区 */}
-      <div className="billList">
-        {dailyBillList.map((item) => {
-          return (
-            <div className="bill" key={item.id}>
-              {/* 图标 */}
-              <div className="detail">
-                {/* billTypeToName-用于中英文名字的适配： */}
-                {/* billTypeToName[英文]=>中文 */}
-                <div className="billType">{billTypeToName[item.useFor]}</div>
+      {/* 7.4 visible的值控制单日列表详细显示区的显示和隐藏 */}
+      {visible && (
+        <div className="billList">
+          {dailyBillList.map((item) => {
+            return (
+              <div className="bill" key={item.id}>
+                {/* 图标 */}
+                <div className="detail">
+                  {/* billTypeToName-用于中英文名字的适配： */}
+                  {/* billTypeToName[英文]=>中文 */}
+                  <div className="billType">{billTypeToName[item.useFor]}</div>
+                </div>
+                <div className={classNames("money", item.type)}>
+                  {item.money.toFixed(2)}
+                </div>
               </div>
-              <div className={classNames("money", item.type)}>
-                {item.money.toFixed(2)}
-              </div>
-            </div>
-          );
-        })}
-      </div>
+            );
+          })}
+        </div>
+      )}
     </div>
   );
 };
