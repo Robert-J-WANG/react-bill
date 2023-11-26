a21bbfba1cc80674c4982137d4b2d1b7085e890e
Robert-J-WANG <robin.nz420@gmail.com>
2023-11-26 16:01:46 +1300

    21-新增账单-静态结构搭建

--- src/router/index.js
+++ src/router/index.js
@@ -1,6 +1,6 @@
 import Layout from "@/pages/Layout";
 import Month from "@/pages/Month";
-import { New } from "@/pages/New";
+import New from "@/pages/New";
 import { Year } from "@/pages/Year";
 import { createBrowserRouter } from "react-router-dom";
 
